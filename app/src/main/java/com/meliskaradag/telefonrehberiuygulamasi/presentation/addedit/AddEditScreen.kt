3b04364db83285e64bfb9b2adf86af03123d5a94
meliskaradag <85998019+ymeliskaradag@users.noreply.github.com>
2025-10-05 16:49:50 +0300

    Coil is updated

--- app/src/main/java/com/meliskaradag/telefonrehberiuygulamasi/presentation/addedit/AddEditScreen.kt
+++ app/src/main/java/com/meliskaradag/telefonrehberiuygulamasi/presentation/addedit/AddEditScreen.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import coil.compose.AsyncImage
+import coil3.compose.AsyncImage
 import com.airbnb.lottie.compose.*
 import com.meliskaradag.telefonrehberiuygulamasi.R
 
